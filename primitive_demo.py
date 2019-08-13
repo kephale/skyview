626e1e8197a51c96fcc314cd046abf4e82653df6
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-08-13 13:22:35 -0400

    Temporarily remove the GLVector call because it fails due to argument/conversion issues

--- primitive_demo.py
+++ primitive_demo.py
@@ -1,18 +1,20 @@
 import pysciview.core as pysv
-from jnius import autoclass
 import random
+import numpy as np
 
-#GLVector = autoclass('cleargl.GLVector')
-
+# Configure pysciview
 psv = pysv.PySciView()
 psv.config()
+
+# Make a SciView instance, this launches ImageJ and SciView
 sv = psv.create()
 
+# Make 10 spheres
 num_spheres = 10
-
 for k in range(num_spheres):
     sphere = sv.addSphere()
-    x = random.uniform(-10,10)
-    y = random.uniform(-10,10)
-    z = random.uniform(-10,10)
-    #sphere.setPosition(GLVector(x,y,z))
+    x = float(random.uniform(-10,10))
+    y = float(random.uniform(-10,10))
+    z = float(random.uniform(-10,10))
+    a = np.array([x,y,z],dtype='float32')
+    #sphere.setPosition(pysv.GLVector(a))
