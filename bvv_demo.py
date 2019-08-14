265f274d5fa71635f48475b606b48a793ed3f0a2
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-08-14 11:08:59 -0400

    Add bvv_demo.py

---
+++ bvv_demo.py
@@ -0,0 +1,39 @@
+# code from https://github.com/imglib/imglyb-learnathon/blob/master/notebooks/bdv/show-numpy-array-in-bdv.ipynb
+
+import h5py
+import os
+import PIL
+import wget
+
+def load_or_download(name):
+    # https://cremi.org
+    
+    target = os.path.join(os.path.expanduser('~'), 'Downloads', f'{name}.hdf')
+    
+    if not os.path.exists(target):
+        source = f'https://cremi.org/static/data/{name}.hdf'.replace('+', '%2B')
+        print(f'Sample {name} not found at {target}. Will download from {source} (this might take a few minutes)')
+        wget.download(source, target)
+        
+    return h5py.File(target, 'r')['volumes/raw'][()]
+
+import imagej
+fiji_directory = './Fiji.app'
+
+ij = imagej.init(fiji_directory,headless=False)
+
+import imglyb
+import imglyb.util as util
+
+name = 'sample_A_20160501'
+data = load_or_download(name)
+img  = imglyb.to_imglib(data)
+#data.shape
+
+#bdv = imglyb.util.BdvFunctions.show(img, name)
+
+from jnius import autoclass, PythonJavaClass, java_method
+
+BvvFunctions = autoclass('bvv.util.BvvFunctions')
+
+bvv = BvvFunctions.show(img, name)
