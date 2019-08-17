8804fdc2d057149dd4327c99409f9f45e06d9237
Jan Funke <funkej@janelia.hhmi.org>
2019-08-17 11:10:00 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+        name='skyview',
+        version='0.1',
+        description='Python interface for SciView.',
+        url='https://github.com/kephale/skyview',
+        author='Kyle Harrington, Jan Funke, Caroline Malin-Mayor',
+        author_email='kephale@kyleharrington.com',
+        license='MIT',
+        packages=[
+            'skyview'
+        ],
+        install_requires=[
+            "numpy",
+            "pyimagej"
+        ]
+)
