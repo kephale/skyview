f4a67935eac583f704c41aa224f4c7f77135c26a
Jan Funke <funkej@janelia.hhmi.org>
2019-08-17 11:04:41 -0400

    Remove fiji zip after extraction

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # If Fiji.app isnt there
 curl -O https://downloads.imagej.net/fiji/latest/fiji-nojre.zip
 unzip fiji-nojre.zip
+rm fiji-nojre.zip
 # If SciView is not installed
 ./Fiji.app/ImageJ-linux64 --update add-update-site sciview https://sites.imagej.net/SciView/
 ./Fiji.app/ImageJ-linux64 --update update
