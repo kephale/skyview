ca033f38cf377fee2dc4ca56fd5a35ce7378b5c1
Jan Funke <funkej@janelia.hhmi.org>
2019-08-18 11:50:58 -0400

    Expose daisy_array_to_img and arraylike_to_img

--- skyview/__init__.py
+++ skyview/__init__.py
@@ -1,4 +1,6 @@
-from .viewer import Viewer
+from .daisy import daisy_array_to_img
 from .events import Events
+from .img_wrapper import arraylike_to_img
+from .viewer import Viewer
 
-__all__ = ['Viewer', 'Events']
+__all__ = ['Viewer', 'Events', 'daisy_array_to_img']
