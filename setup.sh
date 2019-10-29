2c44328a72b2d0e46cca8da509694a7865e67b5d
Ulrik Günther <ulrik@42degreesoffreedom.com>
2019-10-29 13:46:35 -0700

    Make setup.sh script non-destructive (aka, do not delete files), and add platform detection

--- setup.sh
+++ setup.sh
@@ -1,8 +1,37 @@
 # first load Fiji conifg
 # If Fiji.app isnt there
-curl -O https://downloads.imagej.net/fiji/latest/fiji-nojre.zip
-unzip fiji-nojre.zip
-rm fiji-nojre.zip
+if ! unzip -tqq fiji-nojre.zip; then
+  echo
+  echo "--> Downloading Fiji"
+  curl -L -O https://downloads.imagej.net/fiji/latest/fiji-nojre.zip ||
+    die "Could not download Fiji"
+fi
+
+if [ ! -f fiji-nojre.zip ]
+then
+  echo "--> Unpacking Fiji"
+  rm -rf Fiji.app
+  unzip fiji-nojre.zip || die "Could not unpack Fiji"
+fi
+
 # If SciView is not installed
-./Fiji.app/ImageJ-linux64 --update add-update-site sciview https://sites.imagej.net/SciView/
-./Fiji.app/ImageJ-linux64 --update update
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  BINARY=ImageJ-linux64
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  BINARY=Contents/MacOS/ImageJ-macosx
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  BINARY=ImageJ-win64.exe
+elif [[ "$OSTYPE" == "msys" ]]; then
+  BINARY=ImageJ-win64.exe
+elif [[ "$OSTYPE" == "win32" ]]; then
+  BINARY=ImageJ-win32.exe
+else
+  echo "Sorry, but your operating system is not supported by Fiji."
+  exit 1
+fi
+
+echo "--> Adding sciview update site"
+./Fiji.app/$BINARY --update add-update-site sciview https://sites.imagej.net/SciView/
+echo "--> Updating Fiji"
+./Fiji.app/$BINARY --update update
+echo "--> Done."
